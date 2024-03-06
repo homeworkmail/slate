cc579612cab5f75731b1465ce2590665cf74c188
bhsunilvarma13 <mailme4loops@gmail.com>
2024-03-06 09:00:06 +0530

    fix: prisma client global prisma client

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
