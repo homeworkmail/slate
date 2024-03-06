7f854fc7656c55796250d7da2b3c7c3d9ee26007
bhsunilvarma13 <mailme4loops@gmail.com>
2024-03-06 08:57:53 +0530

    Fix: prisma client

--- lib/db.ts
+++ lib/db.ts
@@ -6,4 +6,13 @@ declare global {
 
 export const db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
