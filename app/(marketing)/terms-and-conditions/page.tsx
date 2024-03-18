8cb869bb033c756f1de52b5c4457c7fd7efbf82c
bhsunilvarma13 <mailme4loops@gmail.com>
2024-03-18 09:05:33 +0530

    Update page.tsx

--- app/(marketing)/terms-and-conditions/page.tsx
+++ app/(marketing)/terms-and-conditions/page.tsx
@@ -6,7 +6,7 @@ function page() {
   return (
     <div className="h-full max-w-[600px] mx-auto py-6 space-y-2">
       <h1 className="text-xl font-semibold">
-        Terms and Conditions for ZenVoice
+        Terms and Conditions for Slate
       </h1>
       <Separator />
       <div className="space-y-4 pt-4">
@@ -26,7 +26,7 @@ function page() {
           <div className="font-semibold pb-2">1. Access to the Service:</div>
           <div className="pl-8">
             <li>
-              Access to ZenVoice&apos;s software is granted to the user upon a
+              Access to Slate&apos;s software is granted to the user upon a
               one-time payment, providing unlimited access to the product. This
               includes the ability to connect multiple Stripe accounts (1
               account for the 1st Tier plan, and up to 100 for the 2nd Tier
@@ -42,7 +42,7 @@ function page() {
           <div className="font-semibold pb-2">2. User Data:</div>
           <div className="pl-8">
             <li>
-              ZenVoice collects user email and payment information solely for
+              Slate collects user email and payment information solely for
               the purpose of providing the Service.
             </li>
             <li>
@@ -62,7 +62,7 @@ function page() {
           <div className="font-semibold pb-2">4. Governing Law:</div>
           <div className="pl-8">
             These Terms shall be governed and construed in accordance with the
-            laws of Singapore, without regard to its conflict of law provisions.
+            laws of India, without regard to its conflict of law provisions.
           </div>
         </div>
         <div>
@@ -75,7 +75,7 @@ function page() {
           For any questions or concerns regarding this Privacy Policy, please
           contact us at homeworkmailorg@gmail.com.
         </div>
-        <div className="font-semibold">Thank you for using ZenVoice!</div>
+        <div className="font-semibold">Thank you for using Slate!</div>
       </div>
     </div>
   );
